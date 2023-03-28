6408c4528dda542f72377edc66dcd96e8ea4fa0b
Naywia <48431873+Naywia@users.noreply.github.com>
2023-03-28 13:29:29 +0200

    Update main.py
    Add multiple endpoints

--- API/main.py
+++ API/main.py
@@ -32,25 +32,256 @@ class Archaeologygallery:
                 headers={"WWW-Authenticate": "Bearer"},
             )
         access_token_expires = datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-        accessToken = Authorize().createAccessToken(data={"sub": user[1]}, expires_delta=access_token_expires)
+        accessToken = Authorize().createAccessToken(data={"sub": user[1], "role": user[3]}, expires_delta=access_token_expires)
 
-        content = {"Message": "You've logged in."}
+        content = {"Message": "You've logged in.", "employeeID": user[0]}
         headers = {"Authorization": f"Bearer {accessToken}"}
         return JSONResponse(content=content, headers=headers)
-    
+
+    @api.post("/employeeType", status_code=201, summary="Create new employee type.")
+    async def addEmployeeType(employeeType: EmployeeType, response: Response, token: Token = Depends(Authorize().validateJWT)):
+        """ Endpoint for creating a new user. """
+        Conn().insertEmployeeType(employeeType)
+        return {'message': 'Employee type has been created'}
+
+    # Create a new user. 
+    @api.post("/user", status_code=201, summary="Create new employee.")
+    async def addUser(user: User, response: Response, token: Token = Depends(Authorize().validateJWT)):
+        """ Endpoint for creating a new user. """
+        Conn().insertUser(user.firstName, user.lastName, user.email, Authorize().hashPassword(user.password), user.doorCode, user.phoneNumber, user.employeeTypeID)
+        return {'message': 'User has been created'}
+
+    @api.post("/storage", status_code=201, summary="Create new storage.")
+    async def addStorage(storage: Storage, response: Response, token: Token = Depends(Authorize().validateJWT)):
+        """ Endpoint for creating a new storage room. """
+        Conn().insertStorage(storage.storageName)
+        return {'message': 'Storage has been created'}
+
+    @api.post("/placement", status_code=201, summary="Create new storage placement.")
+    async def addPlacement(placement: StoragePlacement, response: Response, token: Token = Depends(Authorize().validateJWT)):
+        """ Endpoint for creating a new storage placement. """
+        Conn().insertPlacement(placement.shelf, placement.row, placement.storageID)
+        return {'message': 'Placement has been created'}
+
+    @api.post("/artefactType", status_code=201, summary="Create new artefact type.")
+    async def addArtefactType(aType: ArtefactType, response: Response, token: Token = Depends(Authorize().validateJWT)):
+        """ Endpoint for creating a new artefact type. """
+        Conn().insertArtefactType(aType.artefactType)
+        return {'message': 'artefactType has been created'}
+
+    @api.post("/artefact", status_code=201, summary="Create new artefact.")
+    async def addArtefact(artefact: Artefact, response: Response, token: Token = Depends(Authorize().validateJWT)):
+        """ Endpoint for creating a new user. """
+        Conn().insertArtefact(artefact.artefact, artefact.artefactDescription, artefact.artefactTypeID, artefact.placementID)
+        return {'message': 'Artefact has been created'}
+
     # READ
+    @api.get("/user", summary="Get all or one user")
+    def getUser(userID: int = None, token: Token = Depends(Authorize().validateJWT)):
+        """ Endpont for getting all users or one specific user. """
+        # If the userID isn't none.
+        result = []
+        if userID:
+            title = "User"
+            users = Conn().getUserInfo(userID)
+        else:
+            title = "Users"
+            users = Conn().getUsers()
+        i = 0
+        while i < len(users):
+            temp = {}
+            temp['ID'] = users[i][0]
+            temp['FirstName'] = users[i][1]
+            temp['LastName'] = users[i][2]
+            temp['Email'] = users[i][3]
+            temp['PhoneNumber'] = users[i][4]
+            temp['EmployeeType'] = users[i][5]
+
+            result.append(temp)
+            i += 1
+
+        return {title: result}
+
+    @api.get("/storage", summary="Get all or one storage")
+    def getStorage(storageID: int = None, token: Token = Depends(Authorize().validateJWT)):
+        """ Endpont for getting all storages or one specific storage. """
+        # If the storageID isn't none.
+        result = []
+        if storageID:
+            title = "Storage"
+            storages = Conn().getStorageInfo(storageID)
+        else:
+            title = "Storages"
+            storages = Conn().getStorages()
+        i = 0
+        while i < len(storages):
+            temp = {}
+            temp['ID'] = storages[i][0]
+            temp['Name'] = storages[i][1]
+
+            result.append(temp)
+            i += 1
+
+        return {title: result}
+
+    @api.get("/placement", summary="Get all or one storage placement")
+    def getPlacement(placementID: int = None, token: Token = Depends(Authorize().validateJWT)):
+        """ Endpont for getting all placements or one specific placement. """
+        # If the placementID isn't none.
+        result = []
+        if placementID:
+            title = "Placement"
+            placements = Conn().getPlacementInfo(placementID)
+        else:
+            title = "Placements"
+            placements = Conn().getPlacements()
+        i = 0
+        while i < len(placements):
+            temp = {}
+            temp['ID'] = placements[i][0]
+            temp['Storage'] = placements[i][1]
+            temp['Shelf'] = placements[i][2]
+            if placements[i][3]:
+                temp['Row'] = placements[i][3]
+
+            result.append(temp)
+            i += 1
+
+        return {title: result}
+
+    @api.get("/artefactType", summary="Get all or one artefact type")
+    def getArtefactType(artefactTypeID: int = None, token: Token = Depends(Authorize().validateJWT)):
+        """ Endpont for getting all artefact types or one specific artefact type. """
+        # If the artefactTypeID isn't none.
+        result = []
+        if artefactTypeID:
+            title = "Artefact Type"
+            atypes = Conn().getArtefactTypeInfo(artefactTypeID)
+        else:
+            title = "Artefact Types"
+            atypes = Conn().getArtefactTypes()
+        i = 0
+        while i < len(atypes):
+            temp = {}
+            temp['ID'] = atypes[i][0]
+            temp['Type'] = atypes[i][1]
+
+            result.append(temp)
+            i += 1
+
+        return {title: result}
+
+    @api.get("/artefact", summary="Get all or one artefact")
+    def getArtefact(artefactID: int = None, token: Token = Depends(Authorize().validateJWT)):
+        """ Endpont for getting all artefacts or one specific artefact. """
         # If the artefactID isn't none.
+        result = []
         if artefactID:
             title = "Artefact"
-            result
-
+            artefacts = Conn().getArtefactInfo(artefactID)
         else:
             title = "Artefacts"
-            result
-    
+            artefacts = Conn().getArtefacts()
+        i = 0
+        while i < len(artefacts):
+            temp = {}
+            temp['ID'] = artefacts[i][0]
+            temp['Name'] = artefacts[i][1]
+            temp['Description'] = artefacts[i][2]
+            temp['ArtefactType'] = artefacts[i][3]
+            temp['Storage'] = artefacts[i][4]
+            temp['Shelf'] = artefacts[i][5]
+            if artefacts[i][6]:
+                temp['Row'] = artefacts[i][6]
+
+            result.append(temp)
+            i += 1
+
         return {title: result}
 
     # UPDATE
+    @api.patch("/storage", summary="Update storage")
+    def updateStorage(storageID: int, storage: Storage, token: Token = Depends(Authorize().validateJWT)):
+        """ Endpont for updating a storage. """
+        Conn().updateStorage(storageID, storage.storageName)
+        return {'message': 'Storage has been updated'}
+
+    @api.get("/placement", summary="Update storage placement")
+    def getPlacement(placementID: int, placement: UpdateStoragePlacement, token: Token = Depends(Authorize().validateJWT)):
+        """ Endpont for getting all placements or one specific placement. """
+        # If the placementID isn't none.
+        result = []
+        if placementID:
+            title = "Placement"
+            placements = Conn().getPlacementInfo(placementID)
+        else:
+            title = "Placements"
+            placements = Conn().getPlacements()
+        i = 0
+        while i < len(placements):
+            temp = {}
+            temp['ID'] = placements[i][0]
+            temp['Storage'] = placements[i][1]
+            temp['Shelf'] = placements[i][2]
+            if placements[i][3]:
+                temp['Row'] = placements[i][3]
+
+            result.append(temp)
+            i += 1
+
+        return {title: result}
+
+    # Get all or one artefact type.
+    @api.get("/artefactType", summary="Get all or one artefact type")
+    def getArtefactType(artefactTypeID: int = None, token: Token = Depends(Authorize().validateJWT)):
+        """ Endpont for getting all artefact types or one specific artefact type. """
+        # If the artefactTypeID isn't none.
+        result = []
+        if artefactTypeID:
+            title = "Artefact Type"
+            atypes = Conn().getArtefactTypeInfo(artefactTypeID)
+        else:
+            title = "Artefact Types"
+            atypes = Conn().getArtefactTypes()
+        i = 0
+        while i < len(atypes):
+            temp = {}
+            temp['ID'] = atypes[i][0]
+            temp['Type'] = atypes[i][1]
+
+            result.append(temp)
+            i += 1
+
+        return {title: result}
+
+    # Get all or one artefact.
+    @api.get("/artefact", summary="Get all or one artefact")
+    def getArtefact(artefactID: int = None, token: Token = Depends(Authorize().validateJWT)):
+        """ Endpont for getting all artefacts or one specific artefact. """
+        # If the artefactID isn't none.
+        result = []
+        if artefactID:
+            title = "Artefact"
+            artefacts = Conn().getArtefactInfo(artefactID)
+        else:
+            title = "Artefacts"
+            artefacts = Conn().getArtefacts()
+        i = 0
+        while i < len(artefacts):
+            temp = {}
+            temp['ID'] = artefacts[i][0]
+            temp['Name'] = artefacts[i][1]
+            temp['Description'] = artefacts[i][2]
+            temp['ArtefactType'] = artefacts[i][3]
+            temp['Storage'] = artefacts[i][4]
+            temp['Shelf'] = artefacts[i][5]
+            if artefacts[i][6]:
+                temp['Row'] = artefacts[i][6]
+
+            result.append(temp)
+            i += 1
+
+        return {title: result}
 
     # DELETE
     
