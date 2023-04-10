10a23c8e9173f6b0adb9bf97346f74b0928f3c20
Naywia <48431873+Naywia@users.noreply.github.com>
2023-04-10 18:14:46 +0200

    Edit Mosquitto code and add it to main.py

--- API/main.py
+++ API/main.py
@@ -6,10 +6,42 @@ from fastapi import Depends, FastAPI, HTTPException, Response, status
 from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordRequestForm
 
+import paho.mqtt.client as mqtt #import the client
+import json
+
 # Own files.
 from connection import Connection as Conn
 from authorize import Authorize
 from basemodels import *
+
+class Mosquitto:
+    def __init__(self):
+        self.client = mqtt.Client() #create new instance
+        self.client.username_pw_set("user", "P@ssw0rd!")
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.connect("127.0.0.1", 1883) #connect to broker
+
+    # The callback for when the client receives a CONNACK response from the server.
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
+
+        # Subscribing in on_connect() means that if the connection is lost and reconnected, then subscriptions will be renewed.
+        client.subscribe("security")
+
+    # The callback for when a PUBLISH message is received from the server.
+    def on_message(self, client, userdata, msg):
+        payload = msg.payload.decode("utf-8")
+
+        if "{" in payload:
+            data = json.loads(payload)
+
+            print(payload + " received on topic[" + msg.topic + "]")
+
+            Conn().insertLog(data["incident"], data["incidentDate"], data["logTypeID"], data["controllerID"])
+
+        else:
+            print(payload)
 
 # API
 class Archaeologygallery:
--- API/mosquitto.py
+++ API/mosquitto.py
@@ -1,48 +1,41 @@
 import paho.mqtt.client as mqtt #import the client
-from json import loads
-from requests import request as req
+import json
 
-url="http://127.0.0.1:8000"
-host="127.0.0.1"
-username="client"
-password="pass"
 
-# Headers
-headers = {
-    'Authorization': 'Bearer 14c41540aa68c8108e51a7ed017681bdb9dc8e89eeffcd20e673597b84249bc0df01af90ccc91774e559c70ee0c3b6856f0eb930657ddfd62a7631a0f374f608',
-    'Content-Type': 'application/x-www-form-urlencoded'
-}
+class Mosquitto:
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
+    def __init__(self):
+        self.client = mqtt.Client() #create new instance
+        self.client.username_pw_set("user", "P@ssw0rd!")
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.connect("127.0.0.1", 1883) #connect to broker
 
-    # Subscribing in on_connect() means that if the connection is lost and reconnected, then subscriptions will be renewed.
-    client.subscribe("test")
+    # The callback for when the client receives a CONNACK response from the server.
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    payload = msg.payload.decode("utf-8")
+        # Subscribing in on_connect() means that if the connection is lost and reconnected, then subscriptions will be renewed.
+        client.subscribe("security")
 
-    if "{" in payload:
-        data = loads(payload)
+    # The callback for when a PUBLISH message is received from the server.
+    def on_message(self, client, userdata, msg):
+        payload = msg.payload.decode("utf-8")
 
-        print(payload + " received on topic[" + msg.topic + "]")
+        if "{" in payload:
+            data = json.loads(payload)
 
-        payload="zone_id=" + str(data["zone_ID"]) + "&log_action_id=7" + "&temperature=" + data["temperature"] + "&humidity=" + data["humidity"] + "&alarm=0"
+            print(payload + " received on topic[" + msg.topic + "]")
 
-        response=req("POST", url, headers=headers, data=payload)
+            payload="zone_id=" + str(data["zone_ID"]) + "&log_action_id=7" + "&temperature=" + data["temperature"] + "&humidity=" + data["humidity"] + "&alarm=0"
 
-        print(response.text)
+            print(response.text)
 
-    else:
-        print(payload)
+        else:
+            print(payload)
 
-client = mqtt.Client() #create new instance
-client.on_connect = on_connect
-client.on_message = on_message
-client.username_pw_set(username, password)
 
-client.connect(host, 1883) #connect to broker
 
-client.loop_forever()
+
+
+#client.loop_forever()
