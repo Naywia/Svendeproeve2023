228bd145383647104c5759add0c4afee3903cdc6
Naywia <48431873+Naywia@users.noreply.github.com>
2023-04-14 08:08:03 +0200

    Set-up of controller

--- Controller/AGController/src/main.cpp
+++ Controller/AGController/src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
+#include <M5Stack.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+	// put your setup code here, to run once:
+	M5.begin();
+	M5.Lcd.setTextColor(0xE4DFDA);
+	M5.Lcd.setTextSize(3);
+
+	M5.Lcd.drawString("Hello Goat-inator", (int)(M5.Lcd.width()/2), (int)(M5.Lcd.height()/2), 2);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+	// put your main code here, to run repeatedly:
+	M5.update();
+	if (M5.BtnA.wasPressed())
+	{
+		M5.Lcd.clear();
+		M5.Lcd.println("Goat World-inator");
+	}
 }
