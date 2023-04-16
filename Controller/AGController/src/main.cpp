eafc8a7df925138b42b1f27ce324fc715f03af76
Naywia <48431873+Naywia@users.noreply.github.com>
2023-04-16 17:56:01 +0200

    Add setup methods

--- Controller/AGController/src/main.cpp
+++ Controller/AGController/src/main.cpp
@@ -1,23 +1,86 @@
+// Official libaries.
 #include <Arduino.h>
 #include <M5Stack.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <HTTPClient.h>
+
+
+
+// Own Files.
+#include "keyboard.h"
+
+void connectToWifi();
+char *getWifiSSID();
+char *getStorage();
+void setupController();
+
+// MQTT.
+char *mqtt_server;
+char *mqtt_username;
+char *mqtt_psw;
+char *mqtt_topic;
 
 void setup()
 {
 	// put your setup code here, to run once:
 	M5.begin();
 	M5.Lcd.setTextColor(0xE4DFDA);
-	M5.Lcd.setTextSize(3);
+	M5.Lcd.setTextSize(1);
 
-	M5.Lcd.drawString("Hello Goat-inator", (int)(M5.Lcd.width()/2), (int)(M5.Lcd.height()/2), 2);
+	setupController();
 }
 
 void loop()
 {
 	// put your main code here, to run repeatedly:
-	M5.update();
-	if (M5.BtnA.wasPressed())
+	
+}
+
+void connectToWifi(char *ssid, char *psw)
+{
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, psw);
+	while (WiFi.status() != WL_CONNECTED)
 	{
-		M5.Lcd.clear();
-		M5.Lcd.println("Goat World-inator");
+		delay(500);
+		M5.Lcd.print(".");
 	}
+	M5.Lcd.println("Success");
+}
+
+char *getWifiSSID()
+{
+	return "Room Of Requirements";
+}
+
+char *getStorage()
+{
+	HTTPClient http;
+	http.begin("http://192.168.1.100:8000/login");
+	http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+	const char *headerKeys[] = {"Authorization"};
+	http.collectHeaders(headerKeys, 1);
+	int responseCode = http.POST("username=controller&password=contr0llerPassw0rd");
+	String accessToken = http.header("Authorization");
+	http.end();
+
+	http.begin("http://192.168.1.100:8000/storage");
+	http.addHeader("Authorization", accessToken);
+	http.GET();
+	http.end();
+	return "Lager Syd";
+}
+
+void setupController()
+{
+	// Wifi.
+	Keyboard keyboard;
+	char *ssid = getWifiSSID();
+	char *psw = keyboard.start();
+	connectToWifi(ssid, psw);
+	delay(1000);
+	M5.Lcd.clear();
+	char *storage = getStorage();
+	char *controller = keyboard.start();
 }
